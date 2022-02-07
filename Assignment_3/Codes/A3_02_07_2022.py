1917e4a9389e4dd6d68a60d629ba86a60982fab3
whood23 <76736113+whood23@users.noreply.github.com>
2022-02-07 17:08:16 -0500

    cleaned code

--- Assignment_3/Codes/A3_02_07_2022.py
+++ Assignment_3/Codes/A3_02_07_2022.py
@@ -9,10 +9,10 @@ class Layer(ABC):
         self.__prevIn = []
         self.__prevOut = []
 
-    def setPrevIn(self,dataIn):
+    def setPrevIn(self, dataIn):
         self.__prevIn = dataIn
-    
-    def setPrevOut(self,dataOut):
+
+    def setPrevOut(self, dataOut):
         self.__prevOut = dataOut
 
     def getPrevIn(self):
@@ -21,7 +21,7 @@ class Layer(ABC):
     def getPrevOut(self):
         return self.__prevOut
 
-    def backward(self,gradIn):
+    def backward(self, gradIn):
         sg = self.gradient()
         grad = np.zeros((gradIn.shape[0], sg.shape[2]))
         for n in range(gradIn.shape[0]):
@@ -30,21 +30,22 @@ class Layer(ABC):
         return grad
 
     @abstractmethod
-    def forward(self,dataIn):
+    def forward(self, dataIn):
         pass
 
     @abstractmethod
     def gradient(self):
         pass
 
+
 class InputLayer(Layer):
     def __init__(self, dataIn):
         self.dataIn = dataIn
-        self.mean_data = np.mean(dataIn,axis=0)
-        self.std_data = np.std(dataIn,axis=0)
-        self.std_data[self.std_data==0]=1
+        self.mean_data = np.mean(dataIn, axis=0)
+        self.std_data = np.std(dataIn, axis=0)
+        self.std_data[self.std_data == 0] = 1
 
-    def forward(self,dataIn):
+    def forward(self, dataIn):
         self.setPrevIn(dataIn)
         z_scored_data = (dataIn - self.mean_data) / self.std_data
         self.setPrevOut(z_scored_data)
@@ -53,18 +54,15 @@ class InputLayer(Layer):
     def gradient(self):
         pass
 
-    def backward(self,gradIn):
+    def backward(self, gradIn):
         pass
-    
 
-# Activation Layers
-### Edit all the Layer classes
 
 class LinearLayer(Layer):
     def __init__(self):
         super().__init__()
-    
-    def forward(self,dataIn):
+
+    def forward(self, dataIn):
         self.setPrevIn(dataIn)
         linear_data = np.copy(dataIn)
         self.setPrevOut(linear_data)
@@ -72,91 +70,88 @@ class LinearLayer(Layer):
 
     def gradient(self):
         dataIn = np.copy(self.getPrevIn())
-        totRows = np.size(dataIn, axis=0) # Grab the total number of rows
-        totColumns = np.size(dataIn, axis=1) # Grab the total number of columns
-        tensor = np.random.rand(0,totColumns,totColumns) # Create tensor
+        totRows = np.size(dataIn, axis=0)  # Grab the total number of rows
+        totColumns = np.size(dataIn, axis=1)  # Grab the total number of columns
+        tensor = np.random.rand(0, totColumns, totColumns)  # Create tensor
         # Preform Gradient calculation
-        dataIn[dataIn>=0]=1
-        dataIn[dataIn<0]=1
+        dataIn[dataIn >= 0] = 1
+        dataIn[dataIn < 0] = 1
         # Create final matrix and fill tensor
         for row in range(totRows):
-            gradData = np.zeros((totColumns,totColumns))
-            np.fill_diagonal(gradData,dataIn[row])
-            tensor = np.concatenate((tensor,gradData[None]),axis=0)
+            gradData = np.zeros((totColumns, totColumns))
+            np.fill_diagonal(gradData, dataIn[row])
+            tensor = np.concatenate((tensor, gradData[None]), axis=0)
 
         return tensor
-
 
 
 class ReLuLayer(Layer):
     def __init__(self):
         super().__init__()
-    
-    def forward(self,dataIn):
+
+    def forward(self, dataIn):
         self.setPrevIn(dataIn)
         relu_data = np.copy(dataIn)
-        relu_data[relu_data<0]=0
+        relu_data[relu_data < 0] = 0
         self.setPrevOut(relu_data)
         return relu_data
 
     def gradient(self):
         dataIn = np.copy(self.getPrevIn())
-        totRows = np.size(dataIn, axis=0) # Grab the total number of rows
-        totColumns = np.size(dataIn, axis=1) # Grab the total number of columns
-        tensor = np.random.rand(0,totColumns,totColumns) # Create tensor
+        totRows = np.size(dataIn, axis=0)  # Grab the total number of rows
+        totColumns = np.size(dataIn, axis=1)  # Grab the total number of columns
+        tensor = np.random.rand(0, totColumns, totColumns)  # Create tensor
         # Preform Gradient calculation
-        dataIn[dataIn>=0]=1
-        dataIn[dataIn<0]=0
+        dataIn[dataIn >= 0] = 1
+        dataIn[dataIn < 0] = 0
         # Create final matrix and fill tensor
         for row in range(totRows):
-            gradData = np.zeros((totColumns,totColumns))
-            np.fill_diagonal(gradData,dataIn[row])
-            tensor = np.concatenate((tensor,gradData[None]),axis=0)
+            gradData = np.zeros((totColumns, totColumns))
+            np.fill_diagonal(gradData, dataIn[row])
+            tensor = np.concatenate((tensor, gradData[None]), axis=0)
 
         return tensor
 
-        
 
 class SigmoidLayer(Layer):
     def __init__(self):
         super().__init__()
-    
-    def forward(self,dataIn):
+
+    def forward(self, dataIn):
         self.setPrevIn(dataIn)
-        sig_data = 1/(1+np.exp(-dataIn))
+        sig_data = 1 / (1 + np.exp(-dataIn))
         self.setPrevOut(sig_data)
         return sig_data
 
     def gradient(self):
         dataIn = self.getPrevOut()
-        totRows = np.size(dataIn, axis=0) # Grab the total number of rows
-        totColumns = np.size(dataIn, axis=1) # Grab the total number of columns
-        tensor = np.random.rand(0,totColumns,totColumns) # Create an empty tensor
+        totRows = np.size(dataIn, axis=0)  # Grab the total number of rows
+        totColumns = np.size(dataIn, axis=1)  # Grab the total number of columns
+        tensor = np.random.rand(0, totColumns, totColumns)  # Create an empty tensor
         # Create Preform calculation, final matrix, and tensor append 
         for row in range(totRows):
-            diagonal = dataIn[row]*(1-dataIn[row]) # diagonal calculation
-            gradData = np.zeros((totColumns,totColumns))
-            np.fill_diagonal(gradData,diagonal)
-            tensor = np.concatenate((tensor,gradData[None]),axis=0)
+            diagonal = dataIn[row] * (1 - dataIn[row])  # diagonal calculation
+            gradData = np.zeros((totColumns, totColumns))
+            np.fill_diagonal(gradData, diagonal)
+            tensor = np.concatenate((tensor, gradData[None]), axis=0)
         return tensor
-
 
 
 class SoftmaxLayer(Layer):
     def __init__(self):
         super().__init__()
-    
-    def forward(self,dataIn):
+
+    def forward(self, dataIn):
         self.setPrevIn(dataIn)
-        soft_data = np.exp(dataIn)/np.sum(np.exp(dataIn))
+        soft_data = np.exp(dataIn) / np.sum(np.exp(dataIn))
         self.setPrevOut(soft_data)
         return soft_data
 
     def gradient(self):
         dataIn = self.getPrevOut()
-        totRows = np.size(dataIn, axis=0) # Grab the total number of rows
-        totColumns = np.size(dataIn, axis=1) # Grab the total number of columns
-        tensor = np.random.rand(0,totColumns,totColumns) # Create an empty tensor
+        totRows = np.size(dataIn, axis=0)  # Grab the total number of rows
+        totColumns = np.size(dataIn, axis=1)  # Grab the total number of columns
+        tensor = np.random.rand(0, totColumns, totColumns)  # Create an empty tensor
         # Run calculation per row, add expanded matrix to empty tensor
         for row in range(totRows):
             gradData = np.zeros((totColumns, totColumns))
@@ -177,10 +172,10 @@ class SoftmaxLayer(Layer):
 class TanhLayer(Layer):
     def __init__(self):
         super().__init__()
-    
-    def forward(self,dataIn):
+
+    def forward(self, dataIn):
         self.setPrevIn(dataIn)
-        tanh_data = (np.exp(dataIn) - np.exp(-dataIn)) / (np.exp(dataIn)+np.exp(-dataIn))
+        tanh_data = (np.exp(dataIn) - np.exp(-dataIn)) / (np.exp(dataIn) + np.exp(-dataIn))
         self.setPrevOut(tanh_data)
         return tanh_data
 
@@ -199,125 +194,125 @@ class TanhLayer(Layer):
         return tensor
 
 
-
-# Fully Connected Layer
-### Edit all defs except the gradient
-
 class FullyConnectedLayer(Layer):
     def __init__(self, sizeIn, sizeOut):
         self.sizeIn = sizeIn
         self.sizeOut = sizeOut
-        self.weights = np.random.uniform(-0.0001, 0.0001,size=(sizeIn,sizeOut))
-        self.bias = np.random.uniform(-0.0001, 0.0001,size=(1,sizeOut))
+        self.weights = np.random.uniform(-0.0001, 0.0001, size=(sizeIn, sizeOut))
+        self.bias = np.random.uniform(-0.0001, 0.0001, size=(1, sizeOut))
 
     def getWeights(self):
         return self.weights
 
-    def setWeights(self,weights):
+    def setWeights(self, weights):
         self.weights = weights
 
     def getBias(self):
         return self.bias
 
-    def setBias(self,bias):
+    def setBias(self, bias):
         self.bias = bias
 
-    def updateWeights(self,gradIn,eta=0.0001):
+    def updateWeights(self, gradIn, eta=0.0001):
         dJdb = np.sum(gradIn, axis=0) / gradIn.shape[0]
-        dJdw = (np.matmul(self.getPrevIn().T,gradIn) / gradIn.shape[0])
+        dJdw = (np.matmul(self.getPrevIn().T, gradIn) / gradIn.shape[0])
         self.bias = self.bias - (eta * dJdb)
         self.weights = self.weights - (eta * dJdw)
 
-    def forward(self,dataIn):
+    def forward(self, dataIn):
         self.setPrevIn(dataIn)
-        newset = np.matmul(dataIn,self.weights)+self.bias
-        self.setPrevOut(newset)
-        return newset
+        newSet = np.matmul(dataIn, self.weights) + self.bias
+        self.setPrevOut(newSet)
+        return newSet
 
     def gradient(self):
         weights = self.getWeights()
         grad_fc_data = np.transpose(weights)
         return grad_fc_data
 
-    def backward(self,gradIn):
+    def backward(self, gradIn):
         sg = self.gradient()
         grad = np.matmul(gradIn, sg)
         return grad
 
+
 class LeastSquares:
-    def eval(self,y,yhat):
-        j = (y - yhat)**2
+    def eval(self, y, yhat):
+        j = (y - yhat) ** 2
         return j
 
-    def gradient(self,y,yhat):
-        grad_j = -2*(y-yhat)
+    def gradient(self, y, yhat):
+        grad_j = -2 * (y - yhat)
         return grad_j
 
-    def backward(self,gradIn):
+    def backward(self, gradIn):
         pass
+
 
 class LogLoss:
-    def eval(self,y,yhat):
-        j = -1 * ((np.log(yhat+10e-7)*y)+(1-y)*np.log(1-yhat + 10e-7))
+    def eval(self, y, yhat):
+        j = -1 * ((np.log(yhat + 10e-7) * y) + (1 - y) * np.log(1 - yhat + 10e-7))
         return j
 
-    def gradient(self,y,yhat):
-        grad_j = -(y-yhat)/(yhat*(1-yhat) + 10e-7)
+    def gradient(self, y, yhat):
+        grad_j = -(y - yhat) / (yhat * (1 - yhat) + 10e-7)
         return grad_j
 
-    def backward(self,gradIn):
+    def backward(self, gradIn):
         pass
+
 
 class CrossEntropy:
-    def eval(self,y,yhat):
-        j = np.dot(-y,np.log(np.transpose(yhat)))
+    def eval(self, y, yhat):
+        j = np.dot(-y, np.log(np.transpose(yhat)))
         return j
 
-    def gradient(self,y,yhat):
-        grad_j = -y/yhat
+    def gradient(self, y, yhat):
+        grad_j = -y / yhat
         return grad_j
 
-    def backward(self,gradIn):
+    def backward(self, gradIn):
         pass
 
-class runLayers:
-    def __init__(self, X, Y, layers, epochs, eta, eval_method = 'none'):
+
+class RunLayers:
+    def __init__(self, X, Y, layerList, epochs, eta, eval_method='none'):
         self.X = X
         self.Y = Y
-        self.layers = layers
+        self.layers = layerList
         self.epochs = epochs
         self.eta = eta
         self.eval_method = eval_method
 
     def forwardRun(self, X):
         H = X
-        for i in range(len(self.layers)-1):
+        for i in range(len(self.layers) - 1):
             H = self.layers[i].forward(H)
         return H
 
-    def backRun(self,Y, H):
+    def backRun(self, Y, H):
         grad = self.layers[-1].gradient(Y, H)
         for i in range(len(self.layers) - 2, 0, -1):
-            newgrad = self.layers[i].backward(grad)
+            newGrad = self.layers[i].backward(grad)
 
             if isinstance(self.layers[i], FullyConnectedLayer):
                 self.layers[i].updateWeights(grad, self.eta)
 
-            grad = newgrad
+            grad = newGrad
 
-    def mapeRun(self,H):
+    def mapeRun(self, H):
         MAPE = np.mean(np.absolute((self.Y - H) / self.Y))
         return MAPE
 
-    def rmseRun(self,H):
-        RMSE = math.sqrt(np.matmul(np.transpose(self.Y-H), (self.Y-H)) / np.size(H, axis=0))
+    def rmseRun(self, H):
+        RMSE = math.sqrt(np.matmul(np.transpose(self.Y - H), (self.Y - H)) / np.size(H, axis=0))
         return RMSE
 
-    def objfunRun(self,H):
+    def objfunRun(self, H):
         objfunRet = np.mean(self.layers[-1].eval(self.Y, H))
         return objfunRet
 
-    def objSelect(self,H):
+    def objSelect(self, H):
         if self.eval_method == 'none':
             return self.objfunRun(H)
         elif self.eval_method == 'mape':
@@ -335,27 +330,28 @@ class runLayers:
             # Forward
             H = self.forwardRun(self.X)
             # Backwards
-            self.backRun(self.Y,H)
+            self.backRun(self.Y, H)
             error = self.objSelect(H)
             errorStorage.append(error)
             epochStorage.append(j)
 
             if np.absolute(error - prevError) < endDiff:
-                return epochStorage,errorStorage
+                return epochStorage, errorStorage
                 break
 
             prevError = error
 
-        return epochStorage,errorStorage
-    
+        return epochStorage, errorStorage
+
     def classify(self, X):
         classification = self.forwardRun(X)
         classification[classification < 0.5] = 0
         classification[classification >= 0.5] = 1
         return classification
 
-class splitData:
-    def __init__(self, data, percent = 2/3, fsc = 'norm', fscRangeFrom = 0, fscRangeTo = 0):
+
+class SplitData:
+    def __init__(self, data, percent=2 / 3, fsc='norm', fscRangeFrom=0, fscRangeTo=0):
         self.data = data
         self.percent = percent
         np.random.seed(0)
@@ -384,71 +380,71 @@ class splitData:
             return X, Y
 
     def finalSplit(self, train, test):
-        XTrain, YTrain = self.finalSplitCriteria(train)
-        XTest, YTest = self.finalSplitCriteria(test)
-        return XTrain, XTest, YTrain, YTest
+        XTr, YTr = self.finalSplitCriteria(train)
+        XTe, YTe = self.finalSplitCriteria(test)
+        return XTr, XTe, YTr, YTe
 
     def fullSplit(self):
         train, test = self.firstSplit()
-        XTrain, XTest, YTrain, YTest = self.finalSplit(train, test)
-        return XTrain, XTest, YTrain, YTest
+        XTr, XTe, YTr, YTe = self.finalSplit(train, test)
+        return XTr, XTe, YTr, YTe
 
 
 if __name__ == '__main__':
-
-    def plot(xPlotData,yPlotData):
+    def plot(xPlotData, yPlotData):
         plt.plot(xPlotData, yPlotData)
         plt.xlabel('Epoch')
         plt.ylabel('Evaluation Function')
         return plt
 
+
     """
     Part 4
     """
     # Call in data
-    data = np.genfromtxt('mcpd_augmented.csv', delimiter=',', skip_header= True)
+    data = np.genfromtxt('mcpd_augmented.csv', delimiter=',', skip_header=True)
     # Split into train and testing sets
-    spl = splitData(data)
+    spl = SplitData(data)
     XTrain, XTest, YTrain, YTest = spl.fullSplit()
     "MAPE"
     # Call layers
     L1 = InputLayer(XTrain)
-    L2 = FullyConnectedLayer(XTrain.shape[1],1)
+    L2 = FullyConnectedLayer(XTrain.shape[1], 1)
     L3 = LeastSquares()
     layers = [L1, L2, L3]
     "Training"
     # Run Test
-    run = runLayers(XTrain, YTrain, layers, 10000, 0.0001, 'mape')
+    run = RunLayers(XTrain, YTrain, layers, 10000, 0.0001, 'mape')
     epochStorageTrain, errorStorageTrain = run.allRun()
     "Validation"
     # Run Test
-    run = runLayers(XTest, YTest, layers, 10000, 0.0001, 'mape')
+    run = RunLayers(XTest, YTest, layers, 10000, 0.0001, 'mape')
     epochStorageTest, errorStorageTest = run.allRun()
     # Plot
     plot(epochStorageTrain, errorStorageTrain)
     plot(epochStorageTest, errorStorageTest)
-    plt.title('MAPE Vs Epoch')
+    plt.title('Part 4: MAPE Vs Epoch')
     plt.legend(["Training", "Validation"])
     plt.show()
 
     "RMSE"
     # Call Layers
     L1 = InputLayer(XTrain)
-    L2 = FullyConnectedLayer(XTrain.shape[1],1)
+    L2 = FullyConnectedLayer(XTrain.shape[1], 1)
     L3 = LeastSquares()
     layers = [L1, L2, L3]
     "Training"
     # Run Test
-    run = runLayers(XTrain, YTrain, layers, 10000, 0.0001, 'rmse')
+    run = RunLayers(XTrain, YTrain, layers, 10000, 0.0001, 'rmse')
     epochStorageTrain, errorStorageTrain = run.allRun()
     "Validation"
     # Run Test
-    run = runLayers(XTest, YTest, layers, 10000, 0.0001, 'rmse')
+    run = RunLayers(XTest, YTest, layers, 10000, 0.0001, 'rmse')
     epochStorageTest, errorStorageTest = run.allRun()
     # Plot
     plot(epochStorageTrain, errorStorageTrain)
     plot(epochStorageTest, errorStorageTest)
-    plt.title('RMSE Vs Epoch')
+    plt.title('Part 4: RMSE Vs Epoch')
     plt.legend(["Training", "Validation"])
     plt.show()
 
@@ -456,13 +452,13 @@ if __name__ == '__main__':
     Part 5
     """
     # Call in data
-    data = np.genfromtxt('KidCreative.csv', delimiter=',', skip_header= True)
+    data = np.genfromtxt('KidCreative.csv', delimiter=',', skip_header=True)
     # Create train and testing sets
-    spl = splitData(data, 2/3, 'begin', 1, 2)
+    spl = SplitData(data, 2 / 3, 'begin', 1, 2)
     XTrain, XTest, YTrain, YTest = spl.fullSplit()
     # Call layers
     L1 = InputLayer(XTrain)
-    L2 = FullyConnectedLayer(XTrain.shape[1],1)
+    L2 = FullyConnectedLayer(XTrain.shape[1], 1)
     L3 = SigmoidLayer()
     L4 = LogLoss()
     layers = [L1, L2, L3, L4]
@@ -470,15 +466,15 @@ if __name__ == '__main__':
     print("Number of epochs: {}".format(ep))
     "Training"
     # Run test
-    run = runLayers(XTrain, YTrain, layers, ep, 0.0001)
+    run = RunLayers(XTrain, YTrain, layers, ep, 0.0001)
     epochStorageTrain, errorStorageTrain = run.allRun()
     trainClassify = run.classify(XTrain)
     binaryClassify = (YTrain == trainClassify)
-    print("Training Accuracy: {0:.2f}%".format((np.count_nonzero(binaryClassify) / np.size(YTrain, axis = 0)) * 100))
+    print("Training Accuracy: {0:.2f}%".format((np.count_nonzero(binaryClassify) / np.size(YTrain, axis=0)) * 100))
 
     "Validation"
     # Run Test
-    run = runLayers(XTest, YTest, layers, ep, 0.0001)
+    run = RunLayers(XTest, YTest, layers, ep, 0.0001)
     epochStorageTest, errorStorageTest = run.allRun()
     testClassify = run.classify(XTest)
     binaryClassify = (YTest == testClassify)
@@ -486,6 +482,6 @@ if __name__ == '__main__':
     # Plot
     plot(epochStorageTrain, errorStorageTrain)
     plot(epochStorageTest, errorStorageTest)
-    plt.title('Log Loss vs Epoch')
+    plt.title('PArt 5: Log Loss vs Epoch')
     plt.legend(["Training", "Validation"])
     plt.show()
